b27c67d66284c003d71ffd3b68c194875cdc1b7d
Jean-Michael <34538443+DO2JMG@users.noreply.github.com>
2026-06-13 12:56:38 +0200

    Update httpd.c

--- httpd.c
+++ httpd.c
@@ -1,4 +1,9 @@
 
+// Tawhiri - Server and downloader
+//
+// Copyright (C) Jean-Michael (DO2JMG) <info@wettersonde.net>
+//
+// Released under GNU GPL v3 or later
 
 #include "httpd.h"
 #include "models.h"
